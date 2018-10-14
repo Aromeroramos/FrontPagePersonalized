b182f3a947b4d220bee71d1ec3f28a7eee3d7cdc
Alberto Romero Ramos <albertoromeroramos@MacBook-Pro-de-Alberto.local>
2018-10-14 01:58:40 +0100

    adding js document

---
+++ index.js
@@ -0,0 +1,21 @@
+var webFunc1 = "http://www.facebook.com";
+var webFunc2 = "https://twitter.com/";
+var webFunc3 = "https://www.messenger.com/";
+var webFunc4 = "https://web.whatsapp.com/";
+var webFunc5 = "https://www.trello.com/";
+function myFunc1() {
+   window.location = webFunc1;
+   document.getElementById('matrix').src = "images/facebook.svg";
+}
+function myFunc2() {
+   window.location = webFunc2;
+}
+function myFunc3() {
+   window.location = webFunc3;
+}
+function myFunc4() {
+   window.location = webFunc4;
+}
+function myFunc5() {
+   window.location = webFunc5;
+}
