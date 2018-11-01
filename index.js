b13516b740bcb6f73a84d2d9fa44833bdae3645c
Romero Ramos <C0239747@produban.co.uk>
2018-11-01 12:26:57 +0000

    index.html updated

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-var webFunc1 = "http://www.facebook.com";
-var webFunc2 = "https://twitter.com/";
-var webFunc3 = "https://www.messenger.com/";
-var webFunc4 = "https://web.whatsapp.com/";
-var webFunc5 = "https://www.trello.com/";
-function myFunc1() {
-   window.location = webFunc1;
-   document.getElementById("matrix").src="images/facebook.svg";
-   document.write('<img id="myPhoto" width="50" height="50">');
-}
-function myFunc2() {
-   window.location = webFunc2;
-}
-function myFunc3() {
-   window.location = webFunc3;
-}
-function myFunc4() {
-   window.location = webFunc4;
-}
-function myFunc5() {
-   window.location = webFunc5;
-}
+fetch('http://www.google.com')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
