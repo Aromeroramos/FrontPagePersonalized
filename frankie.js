80ae6acfb4a7970c91f85a9a67db6a12b155cfb8
Franarroutado <franarroutado@yahoo.es>
2018-11-09 17:37:40 +0000

    Frankie's bookmarks

---
+++ frankie.js
@@ -0,0 +1,57 @@
+var myArr = [{
+    headername: "Estimation Team",
+    myObject: [{
+        name: "Web1",
+        web: "",
+        imagen: ""
+    }, {
+        name: "Web2",
+        web: "",
+        imagen: ""
+    }, {
+        name: "Web3",
+        web: "",
+        imagen: ""
+    }],
+    headimg: "images/social.svg"
+}, {
+    headername: "Develop",
+    myObject: [{
+        name: "gitHub",
+        web: "https://github.com/",
+        imagen: "images/github.svg"
+    }, {
+        name: "w3schools",
+        web: "https://www.w3schools.com/nodejs/nodejs_get_started.asp",
+        imagen: ""
+    }, {
+        name: "Can I Use",
+        web: "https://caniuse.com/",
+        imagen: ""
+    }, {
+        name: "Jira",
+        web: "",
+        imagen: ""
+    }],
+    headimg: "images/4chan.svg"
+}, {
+    headername: "Personal",
+    myObject: [{
+        name: "Google translate",
+        web: "https://translate.google.co.uk/",
+        imagen: "images/googletranslate.svg"
+    }, {
+        name: "Wordreference",
+        web: "http://www.wordreference.com/",
+        imagen: ""
+    }, {
+        name: "Bank-Holidays",
+        web: "https://www.gov.uk/bank-holidays",
+        imagen: ""
+    }, {
+        name: "Trello",
+        web: "https://www.trello.com/",
+        imagen: "images/trello.svg"
+    }],
+    headimg: "images/diamond.svg"
+}];
